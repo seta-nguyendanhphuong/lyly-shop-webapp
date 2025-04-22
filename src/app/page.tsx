f8e4840b271cbf2b234c2b974d6b435a6a96573e
Christian Do <chung.do@aeris.net>
2025-04-22 10:20:37 +0700

    update: change view of homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -93,23 +93,141 @@ export default function Home() {
     );
   };
 
+  const renderSponsor = () => {
+    return (
+      <div className="min-h-[100px] py-14">
+        <h1 className="text-center pb-10 text-2xl font-bold text-[#003459]">
+          Proud to sponsor for{" "}
+        </h1>
+        <div className="flex w-[81%] mx-auto justify-around items-stretch">
+          <div>
+            <img
+              src="https://ntthnue.edu.vn/uploads/Images/2023/V3/Logo_Web_Bo%20tr%E1%BA%AFng.png"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/vi/3/30/Logo-NEU.PNG"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://ntthnue.edu.vn/uploads/Images/2023/V3/Logo_Web_Bo%20tr%E1%BA%AFng.png"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/vi/3/30/Logo-NEU.PNG"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://ntthnue.edu.vn/uploads/Images/2023/V3/Logo_Web_Bo%20tr%E1%BA%AFng.png"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/vi/3/30/Logo-NEU.PNG"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://ntthnue.edu.vn/uploads/Images/2023/V3/Logo_Web_Bo%20tr%E1%BA%AFng.png"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/vi/3/30/Logo-NEU.PNG"
+              alt=""
+              width={"65px"}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const renderService = () => {
+    return (
+      <div className="min-h-[100px] py-14">
+        <div className="flex w-[81%] mx-auto justify-around items-stretch">
+          <div className="flex flex-col items-center min-h-2">
+            <CiDeliveryTruck fontSize={"60px"} color={"gray"} />
+            <div className="text-[20px] font-medium text-gray-500 mt-3">
+              Vân chuyển nhanh
+            </div>
+          </div>
+
+          <div className="flex flex-col max-h-[200px] pl-[1px] bg-gray-300 border-1 border-black border-solid"></div>
+
+          <div className="flex flex-col items-center min-h-2">
+            <IoChatbubblesOutline fontSize={"60px"} color={"gray"} />
+            <div className="text-[20px] font-medium text-gray-500 mt-3">
+              Tư vấn nhiệt tình
+            </div>
+          </div>
+
+          <div className="flex flex-col max-h-[200px] pl-[1px] bg-gray-300 border-1 border-black border-solid"></div>
+
+          <div className="flex flex-col items-center min-h-2">
+            <TbHours24 fontSize={"60px"} color={"gray"} />
+            <div className="text-[20px] font-medium text-gray-500 mt-3">
+              Hộ Trợ 24/7
+            </div>
+          </div>
+
+          <div className="flex flex-col max-h-[200px] pl-[1px] bg-gray-300 border-1 border-black border-solid"></div>
+
+          <div className="flex flex-col items-center min-h-2">
+            <IoIosSwap fontSize={"60px"} color={"gray"} />
+            <div className="text-[20px] font-medium text-gray-500 mt-3">
+              Đổi & Trả
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div>
       {renderBanner()}
-
       <div className="min-h-[900px] w-full max-w-[1440px] mx-auto">
         <div className="w-[81%] mx-auto">
+          {renderSponsor()}
+          <AdvertisementBanner />
           {renderTypeProduct("Sản phẩm được thuê nhiều")}
           {renderProducts()}
         </div>
       </div>
 
-      <div className="min-h-[900px] w-full max-w-[1440px] mx-auto">
+      <div className="min-h-[200px] w-full max-w-[1440px] mx-auto">
         <div className="w-[81%] mx-auto">
-          {renderTypeProduct("Sản phẩm của chúng tôi")}
+          {/* {renderTypeProduct("Sản phẩm của chúng tôi")} */}
           {/* {renderProducts()} */}
-          <AdvertisementBanner />
-          {renderProducts()}
+          {/* {renderProducts()} */}
+          {renderService()}
         </div>
       </div>
     </div>
